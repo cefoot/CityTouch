a8f60894e28a5c1bb2fa2ae8b833692ffd486c75
Chris Papenfuß <chris.papenfuss@data-experts.de>
2023-01-13 20:48:16 -0500

    helper script

---
+++ Assets/Editor/Tools.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Tools : MonoBehaviour
+{
+    [MenuItem("Tools/Hirachy/ShowAllElements", false, 0)]
+    public static void UnHideObjects()
+    {
+        var scene = SceneManager.GetActiveScene();
+        foreach (var obj in scene.GetRootGameObjects())
+        {
+            UnHideObjects(obj);
+        }
+    }
+
+    private static void UnHideObjects(GameObject obj)
+    {
+        obj.hideFlags = HideFlags.None;
+        foreach (Transform item in obj.transform)
+        {
+            UnHideObjects(item.gameObject);
+        }
+    }
+}
