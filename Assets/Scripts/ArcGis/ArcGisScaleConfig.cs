be79d51ae5df65af8410ee92588443b882ef73a7
Chris Papenfuß <chris.papenfuss@data-experts.de>
2023-01-14 08:26:19 -0500

    tool to apply ArcGisFix

---
+++ Assets/Scripts/ArcGis/ArcGisScaleConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Esri.ArcGISMapsSDK.Renderer.Renderables;
+using Esri.GameEngine;
+using Esri.GameEngine.Map;
+using UnityEngine;
+
+public class ArcGisScaleConfig : MonoBehaviour
+{
+    public Vector3 TargetScale = new Vector3(0.0002f, 0.0002f, 0.0002f);
+
+    private void OnEnable()
+    {
+        RenderZoomLevel.Zoom = TargetScale;
+    }
+
+}
